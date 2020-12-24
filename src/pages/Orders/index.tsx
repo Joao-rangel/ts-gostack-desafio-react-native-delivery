e2674a2c9bc545ee63b07500d28407524bdf5933
Joao Rangel <joao.vran@gmail.com>
2020-12-23 21:56:31 -0300

    list orders

--- src/pages/Orders/index.tsx
+++ src/pages/Orders/index.tsx
@@ -32,7 +32,7 @@ const Orders: React.FC = () => {
 
   useEffect(() => {
     async function loadOrders(): Promise<void> {
-      // Load orders from API
+      await api.get('/orders').then(({ data }) => setOrders(data));
     }
 
     loadOrders();
